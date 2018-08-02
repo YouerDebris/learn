e1e4406dd7404a290562f1dacad3d2c03bbb18aa
YouerSu <youersu@outlook.com>
2018-08-02 18:03:28 +0800

    add

--- Haskell/if.hs
+++ Haskell/if.hs
@@ -2,3 +2,10 @@ sayHello name =
   if name=="Youer"
   then ("Hello,"++name)
   else "Who are you?"
+
+greet :: [String] -> String
+
+greet (x:xs) =
+  if x=="Youer"
+  then sayHello x 
+  else greet (tail xs)
