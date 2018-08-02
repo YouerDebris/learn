5e0deffae890331b15d3aeb5a137ad85998a2176
YouerSu <youersu@outlook.com>
2018-08-02 16:23:45 +0800

    add

---
+++ Haskell/if.hs
@@ -0,0 +1,4 @@
+sayHello name =
+  if name=="Youer"
+  then ("Hello,"++name)
+  else "Who are you?"
---
+++ Haskell/plan.hs
@@ -0,0 +1,15 @@
+type PlanName = String
+type Partner = [String]
+type Todo = String
+
+data Plan = Plan {
+  planName :: PlanName,
+  partner :: Partner,
+  todo :: Todo
+                 }
+
+data Activity = DoHomeWork
+                             | GoForAWalk
+                             | Learn
+            
+            
