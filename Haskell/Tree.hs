271e2674578c7eb6e8bbc07d6ef28f3017f995c6
YouerSu <youersu@outlook.com>
2018-08-16 21:27:45 +0800

    add

--- Haskell/Tree.hs
+++ Haskell/Tree.hs
@@ -1,8 +1,14 @@
 module Tree where
 
-data Tree value  =
-  Node value (Tree value) (Tree value)
-  |Empty
-    deriving(Show)
+data Tree value = Node value (Tree value) (Tree value)|Nil
+    deriving(Show,Eq)
 
---while f tree =  
+value (Node v _ _) = v
+--first :: (Tree t -> a) -> Tree t -> a
+--first :: Tree a -> [b]
+
+first Nil = []
+first tree = value tree++first (left tree)++first (right tree)
+  where
+    left (Node _ value _) = value
+    right (Node _ _ value) = value
