b4f9665820c08a22fed20f0983ce543c15bb22ed
YouerSu <youersu@outlook.com>
2018-08-18 17:26:48 +0800

    add

--- Haskell/Tree.hs
+++ Haskell/Tree.hs
@@ -3,12 +3,17 @@ module Tree where
 data Tree value = Node value (Tree value) (Tree value)|Nil
     deriving(Show,Eq)
 
+fac value = Node value Nil Nil
+
 value (Node v _ _) = v
---first :: (Tree t -> a) -> Tree t -> a
---first :: Tree a -> [b]
+left (Node _ value _) = value
+right (Node _ _ value) = value
 
 first Nil = []
-first tree = value tree++first (left tree)++first (right tree)
-  where
-    left (Node _ value _) = value
-    right (Node _ _ value) = value
+first tree = value tree:first (left tree)++first (right tree)
+
+second Nil = []
+second tree = second (left tree)++value tree:second (right tree)
+
+third Nil = []
+third tree = third (left tree)++third (right tree)++value tree:[]
