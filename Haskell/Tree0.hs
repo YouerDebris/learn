9f9a4cc87441126252c344f6631b6edcf2b828eb
YouerSu <youersu@outlook.com>
2018-11-28 20:33:23 +0800

    add

---
+++ Haskell/Tree0.hs
@@ -0,0 +1,14 @@
+module Tree0 where
+
+import Tree
+import Data.List
+
+balance :: Ord a => [a] -> Tree a
+balance [] = Nil
+balance list = Node (head rh) (balance lf) (balance (drop 1 rh))
+  where 
+    l = sort list
+    t = splitList
+    lf = fst t
+    rh = snd t
+    splitList = splitAt ((length l) `div` 2) l
